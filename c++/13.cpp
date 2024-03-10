8034a5450cce02a3d2f41ba969aa5df77ea89cf4
datnguyen2k3 <thanhdatmu2003@gmail.com>
2024-03-10 17:24:54 +0700

    13 cpp

---
+++ c++/13.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int MOD = 1000000007;
+
+int getLongestPalindromeString(string str) {
+    int maxPalindromeLength = 0;
+
+    for (int i = 0; i < str.size(); i++) {
+        int left = i;
+        int right = i;
+
+        while (left >= 0 && right < str.size() && str[left] == str[right]) {
+            maxPalindromeLength = max(maxPalindromeLength, right - left + 1);
+            left--;
+            right++;
+        }
+
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < str.size() && str[left] == str[right]) {
+            maxPalindromeLength = max(maxPalindromeLength, right - left + 1);
+            left--;
+            right++;
+        }
+    }
+
+    return maxPalindromeLength;
+}
+
+int main() {
+
+    string str = "babad";
+
+    cout << getLongestPalindromeString(str) << endl; // 3
+    return 0;
+}
