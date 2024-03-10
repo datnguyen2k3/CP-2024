638c1b650b08d39e7e063ac570a47dcfc1bd165e
datnguyen2k3 <thanhdatmu2003@gmail.com>
2024-03-10 15:44:53 +0700

    config c++17

--- python/2-bincoef.py
+++ python/2-bincoef.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 def comb(n, k):
     if k == 0 or k == n:
         return 1
-    if k == 1 or k == n - 1:
+    if k == 1:
         return n
     
     return comb(n - 1, k - 1) + comb(n - 1, k)
