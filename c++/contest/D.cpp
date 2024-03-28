10680eb1bec4606b456ce39a2780219972e9159f
datnguyen2k3 <thanhdatmu2003@gmail.com>
2024-03-28 19:56:48 +0700

    d e

---
+++ c++/contest/D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int MOD = 1000000007;
+
+int get_fibonacci(long long n, int mod) {
+
+}
+
+int main() {
+    return 0;
+}
---
+++ c++/contest/E.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int MOD = 1000000007;
+
+long long get_isosceles_triangles(vector<int> edges) {
+    unordered_map<int, int> edge_count;
+    for (int edge : edges) {
+        edge_count[edge]++;
+    }
+
+    long long res = 0;
+    for (auto& [edge, count] : edge_count) {
+        if (count < 2) {
+            continue;
+        }
+
+        res += (long long) count * (count - 1) / 2 * (edges.size() - count);
+        res += (long long) count * (count - 1) * (count - 2) / 6;
+    }
+
+    return res;
+} 
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> edges(n);
+    for (int i = 0; i < n; i++) {
+        cin >> edges[i];
+    }
+
+    cout << get_isosceles_triangles(edges) << endl;
+
+    return 0;
+}
